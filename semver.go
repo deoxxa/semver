af3cfc2f4943c7fb58b68cff8745c4e43aea29dd
deoxxa <deoxxa@fknsrs.biz>
2015-04-23 09:00:59 +1000

    add sortable version list

--- semver.go
+++ semver.go
@@ -1,1 +1,7 @@
 package semver
+
+type List []Version
+
+func (l List) Len() int           { return len(l) }
+func (l List) Swap(i, j int)      { l[i], l[j] = l[j], l[i] }
+func (l List) Less(i, j int) bool { return l[i].LessThan(l[j]) }
